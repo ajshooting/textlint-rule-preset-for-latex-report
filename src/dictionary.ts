a1835e8dc84556374c1633c5cd8259f764e1aec6
ajshooting <ajshooting.aj@gmail.com>
2024-10-25 10:51:14 +0900

    辞書を正規表現に

--- src/dictionary.ts
+++ src/dictionary.ts
@@ -5,14 +5,12 @@ export interface DictionaryEntry {
     correct: string;
 }
 
-// ここ正規表現対応がいいです
-
 export const dictionary: DictionaryEntry[] = [
-    { incorrect: '象', correct: '像' },
+    { incorrect: '(?<!印)(?<!抽)(?<!対)(?<!事)(?<!気)(?<!現)象', correct: '像' },
+    { incorrect: '対象実験|対称実験', correct: '対照実験' },
     { incorrect: '車体', correct: '斜体' },
-    { incorrect: '保管', correct: '補間' },
-    { incorrect: '補完', correct: '補間' },
-    { incorrect: '入社', correct: '入射' },
-    { incorrect: '指揮', correct: '式' },
-    { incorrect: '四季', correct: '式' },
+    { incorrect: '保管|補完|補巻', correct: '補間' },
+    { incorrect: '入社|入車|入舎', correct: '入射' },
+    { incorrect: '指揮|四季', correct: '式' },
+    { incorrect: '(?<!巨)(?<!無量)大数', correct: '対数' },
 ];
