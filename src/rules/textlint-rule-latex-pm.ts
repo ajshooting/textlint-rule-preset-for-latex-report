23516d93ddef8c0389f48e8d470f64ade6d2780a
ajshooting <ajshooting.aj@gmail.com>
2024-11-03 18:19:52 +0900

    change:pm/不確かさの桁数が適切かどうか

--- src/rules/textlint-rule-latex-pm.ts
+++ src/rules/textlint-rule-latex-pm.ts
@@ -19,23 +19,41 @@ const report: TextlintRuleReporter<Options> = (context, options = {}) => {
             const pmMatches = Array.from(text.matchAll(pmRegex));
             for (const match of pmMatches) {
                 const index = match.index ?? 0;
+
                 // 小数点以下の桁数が一致していない
                 if (match[2].length !== match[4].length) {
                     const matchRange = [index, index + match[0].length] as const;
-                    const ruleError = new RuleError('小数点以下の桁数が揃っていません', {
-                        padding: locator.range(matchRange),
-                    });
+                    const ruleError = new RuleError(
+                        '小数点以下の桁数が揃っていません : ' + match[2].length + '/' + match[4].length,
+                        {
+                            padding: locator.range(matchRange),
+                        }
+                    );
                     report(node, ruleError);
                 }
-                // 有効数字とり過ぎ
-                const lastDigit = match[4].replace(/^0+/, '');
-                if ((match[3] == '0' && lastDigit.length > 2) || (match[3] !== '0' && match[4].length > 2)) {
+
+                // 有効数字の桁数が適切かどうか
+                const lastDigit = Number(match[4].replace(/^0+/, ''));
+                if (lastDigit >= 100) {
                     const matchRange = [index, index + match[0].length] as const;
-                    const ruleError = new RuleError('有効数字を取り過ぎているかもしれません :' + lastDigit.length + '桁', {
+                    const ruleError = new RuleError('有効数字は最大2桁が望ましいです :' + lastDigit.toString().length + '桁', {
+                        padding: locator.range(matchRange),
+                    });
+                    report(node, ruleError);
+                } else if (lastDigit >= 40) {
+                    const matchRange = [index, index + match[0].length] as const;
+                    const ruleError = new RuleError('4-9の不確かさは1桁が望ましいです :' + lastDigit.toString().length + '桁', {
+                        padding: locator.range(matchRange),
+                    });
+                    report(node, ruleError);
+                } else if (lastDigit <= 3) {
+                    const matchRange = [index, index + match[0].length] as const;
+                    const ruleError = new RuleError('1-3の不確かさは2桁が望ましいです :' + lastDigit.toString().length + '桁', {
                         padding: locator.range(matchRange),
                     });
                     report(node, ruleError);
                 }
+
                 // 整数部分が大きすぎ
                 if (match[1].length > 2 && match[3].length > 2) {
                     const matchRange = [index, index + match[0].length] as const;
