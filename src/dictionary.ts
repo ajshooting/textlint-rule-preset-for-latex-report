77a9551ed391a7043875a216fb989c9ae32f5fdf
ajshooting <ajshooting.aj@gmail.com>
2025-06-24 23:42:28 +0900

    辞書の修正と追加

--- src/dictionary.ts
+++ src/dictionary.ts
@@ -6,7 +6,7 @@ export interface DictionaryEntry {
 }
 
 export const dictionary: DictionaryEntry[] = [
-    { incorrect: '(?<!印)(?<!抽)(?<!対)(?<!事)(?<!気)(?<!現)象', correct: '像' },
+    { incorrect: '(?<!印|抽|対|事|気|現)象', correct: '像' },
     { incorrect: '対象実験|対称実験', correct: '対照実験' },
     { incorrect: '車体', correct: '斜体' },
     { incorrect: '保管|補完|補巻', correct: '補間' },
@@ -28,13 +28,21 @@ export const dictionary: DictionaryEntry[] = [
     { incorrect: '金属級(?!シリコン|Si)', correct: '金属球' },
     { incorrect: 'されいる', correct: 'されている' },
     { incorrect: 'ボルタ振り子', correct: 'ボルダ振り子' },
-    { incorrect: '一階', correct: '一回' },
+    { incorrect: '一階(?!線形|微分)', correct: '一回' },
     { incorrect: 'っっ', correct: 'っ' },
     { incorrect: '実行値', correct: '実効値' },
     { incorrect: '制限波', correct: '正弦波' },
     { incorrect: '移送', correct: '位相' },
-    // { incorrect: '発信', correct: '発振' },
-    // 機械と器械
+    { incorrect: '(?<!\\|arc)sin', correct: '\\sin' },
+    { incorrect: '(?<!\\|arc)cos', correct: '\\cos' },
+    { incorrect: '(?<!\\|arc)tan', correct: '\\tan' },
+    { incorrect: '(?<!\\|arc)csc', correct: '\\csc' },
+    { incorrect: '(?<!\\|arc)sec', correct: '\\sec' },
+    { incorrect: '(?<!\\|arc)cot', correct: '\\cot' },
+    // 発信/発振
+    // 機械/器械
+    // 電装/伝送
+    // 開放/解放
     // { incorrect: 'も止まった', correct: '求まった' },
     // 動形詞だと"組み合わせ"、名詞だと"組合せ"が正しいらしい
     // { incorrect: '組合せ|組合わせ', correct: '組み合わせ' },
